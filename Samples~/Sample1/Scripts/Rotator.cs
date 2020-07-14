effedc258f836a72fa09799167283d7c0f4bc5f8
paSushkov <63598866+paSushkov@users.noreply.github.com>
2020-07-14 23:23:30 +0300

    Add files via upload

---
+++ Samples~/Sample1/Scripts/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float RotationSpeed;
+
+    private Transform transform;
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, RotationSpeed * Time.deltaTime);
+    }
+}
